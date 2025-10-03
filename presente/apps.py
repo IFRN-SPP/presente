41ec4958af49f2f28355a80497e7263aa4c5390b
Diego Cirilo <dvcirilo@gmail.com>
2025-10-03 16:18:15 -0300

    ruff

--- presente/apps.py
+++ presente/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PresenteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'presente'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "presente"
